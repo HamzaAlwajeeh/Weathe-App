4c06f168d449aed6e93b0642226f0a03cf4cc4c4
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-07-10 12:19:22 +0300

    Create web service api conection with model

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,1 +1,2 @@
+const String kBaseUrl = 'https://api.weatherapi.com/v1';
 const String kApiKey = 'b3ee520dfe304d2082c80849251007';
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,1 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/constants/constants.dart';
+import 'package:weather_app/models/weather_model.dart';
 
+class WeatherService {
+  final Dio dio;
+
+  WeatherService({required this.dio});
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$kBaseUrl/forecast.json?key=$kApiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+
+      return weatherModel;
+    } on DioException catch (e) {
+      final errorMessage = e.response?.data['error']['message'] ??
+          'oops there is an error , try again later';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('oops there is an error , try again later');
+    }
+  }
+}
